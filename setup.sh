b0064a8a6cd6f1c45ef1da8286dae0c8a6916914
JayceDang <jaycedang0601@gmail.com>
2024-12-03 16:32:34 +0700

    Update Setup

--- setup.sh
+++ setup.sh
@@ -2,28 +2,25 @@
 echo "Build Docker"
 
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt update
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 
 sudo systemctl start docker
-
 sudo systemctl status docker
 
-#Tuy chon
+# Tùy chọn: Cài đặt Docker Compose (nếu cần)
 #sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #sudo chmod +x /usr/local/bin/docker-compose
 
 sudo usermod -aG docker $USER
 
 echo "Build DVWA"
-
 docker pull vulnerables/web-dvwa
-
 docker run -d -p 80:80 vulnerables/web-dvwa
